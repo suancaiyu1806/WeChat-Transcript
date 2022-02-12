a1abb55ad919ea09fad50012842bd5c7bee20f7a
vectordong <2297242763@qq.com>
2022-02-12 18:09:20 +0800

    fix: 完成查找逻辑

--- miniprogram/pages/search/search.js
+++ miniprogram/pages/search/search.js
@@ -5,50 +5,20 @@ Page({
      * 页面的初始数据
      */
     data: {
-        
+        pageNum: 0,
         items: [{
-            id:'',
+            id: '',
             customer: '榕城 B07--15252552155',
             product: '某年某日0添蔗糖乳茶白桃乌龙（港式原味）',
             specification: '1*24',
             purchase_price: '64',
             unit_price: '64 64 64 64 64',
             selling_price: '64',
-            wholesale_price:'64 64',
+            wholesale_price: '64 64',
         }],
         localSchools: [],
         searching: false,
     },
-
-    onSearch() {
-        this.data.value.length > 1 ?
-            this.onQuery(this.data.value) : '';
-        console.log('搜索' + this.data.value);
-    },
-
-
-    onQuery() {
-        let queryItems = [];
-        let reg = new RegExp(this.data.value);
-    
-        this.setData({
-          items: []
-        })
-    
-        this.setData({
-          searching:true
-        })
-    
-        this.data.localItems.map(item => {
-          if (reg.test(item.name)) {
-            queryItems.push(item);
-          }
-        })
-    
-        this.setData({
-            items: queryItems
-        })
-      },
 
     /**
      * 生命周期函数--监听页面加载
@@ -61,7 +31,7 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+        this.onQuery();
     },
 
     /**
@@ -69,6 +39,67 @@ Page({
      */
     onShow: function () {
 
+    },
+
+    /**
+     * 自定义函数
+     */
+
+    onChange(e) {
+        this.setData({
+            value: e.detail,
+        });
+    },
+    onSearch() {
+        this.data.value?.length > 1 ?
+            this.onQuery(this.data.value) : '';
+        console.log('搜索' + this.data.value);
+    },
+    onQuery(pageNum = 0) {
+
+        const db = wx.cloud.database()
+        // 查询当前用户所有的 counters
+        if (this.data.value) {
+            this.setData({
+                items: [],
+                searching: true
+            })
+            db.collection('items').where({
+                product: db.RegExp({
+                    regexp: '.*' + this.data.value + '.*',
+                    options: 's',
+                })
+            }).get({
+                success: res => {
+                    this.setData({
+                        items: res.data,
+                    })
+                },
+                fail: err => {
+                    wx.showToast({
+                        icon: 'none',
+                        title: '查询记录失败'
+                    })
+                }
+            })
+        } else {
+            db.collection('items')
+                .skip(pageNum * 10) //从第几个数据开始
+                .limit(pageNum * 10 + 10)
+                .get({
+                    success: res => {
+                        this.setData({
+                            items: res.data,
+                        })
+                    },
+                    fail: err => {
+                        wx.showToast({
+                            icon: 'none',
+                            title: '查询记录失败'
+                        })
+                    }
+                })
+        }
     },
 
     /**
@@ -96,13 +127,21 @@ Page({
      * 页面上拉触底事件的处理函数
      */
     onReachBottom: function () {
+        if (!this.data.searching) {
+            console.log("触底触底");
+            const nextItems = this.onQuery(this.data.pageNum + 1);
+            this.setData({
+                pageNum: this.data.pageNum + 1,
+                items: [...this.data.items,...nextItems]|| [],
+            });
+        } else {
 
+        }
     },
 
     /**
      * 用户点击右上角分享
      */
     onShareAppMessage: function () {
-
     }
 })
